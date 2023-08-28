b1c74f011c745b46cf94874ffcbac7d24452059f
山本逸朗 <140079920+B-2ST@users.noreply.github.com>
2023-08-28 21:35:45 +0900

    Add files via upload

---
+++ helloworldの操作.java
@@ -0,0 +1,15 @@
+package Homework11;
+
+public class helloworldの操作 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello!" );
+		System.out.println("My name is Itsuro" );
+		System.out.println("I live in TOCHIGI" );
+		System.out.println("Thank you!" );
+		
+	
+	}
+
+}
